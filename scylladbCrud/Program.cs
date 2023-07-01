90feeeace03a5cf79dd0d19f36499ca0b14ecf8e
Josué Corrêa <josuejtc@gmail.com>
2023-07-01 11:11:04 -0300

    first version

---
+++ scylladbCrud/Program.cs
@@ -0,0 +1,22 @@
+﻿using Cassandra;
+using System;
+
+namespace scylladbCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cluster = Cluster.Builder()
+                .AddContactPoints("172.17.0.2:9042")
+                .Build();
+
+
+            var session = cluster.Connect("sample_keyspace");
+
+            Console.ReadKey();
+        }
+    }
+}
